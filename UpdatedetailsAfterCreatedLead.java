ea5031ebdb8056cd64405edd4a3de4c77573a358
santhosh-15-R <126150932+santhosh-15-R@users.noreply.github.com>
2023-04-04 19:51:33 +0530

    Create UpdatedetailsAfterCreatedLead.java

---
+++ UpdatedetailsAfterCreatedLead.java
@@ -0,0 +1,34 @@
+package sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class UpdateLeadTask {
+    public static void main(String[] args) throws InterruptedException {
+
+        EdgeDriver driver = new EdgeDriver(); //initiate driver
+
+
+        driver.manage().window().maximize();
+        driver.get("http://leaftaps.com/opentaps/control/login");
+
+        driver.findElement(By.id("username")).sendKeys("DemoCsr");
+        driver.findElement(By.id("password")).sendKeys("crmsfa");
+        driver.findElement(By.className("decorativeSubmit")).click();
+        driver.findElement(By.linkText("CRM/SFA")).click();
+        driver.findElement(By.linkText("Leads")).click();
+        driver.findElement(By.linkText("Create Lead")).click();
+        driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TesLeaf");
+        driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Santhosh");
+        driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+        driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("santhoshsanty15022000@gmail.com");
+        driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("6380079031");
+        driver.findElement(By.name("submitButton")).click();
+        driver.findElement(By.linkText("Edit")).click();
+        WebElement Update = driver.findElement(By.id("updateLeadForm_companyName"));
+        Update.clear();
+        Update.sendKeys("TestLeaf");
+        driver.findElement(By.name("submitButton")).click();
+    }
+}
